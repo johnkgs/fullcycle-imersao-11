1587bc528ff6810fbff2121cb7eedada0d435e3f
JohnKGS <johnkelvinsouza@hotmail.com>
2022-12-05 18:38:50 -0300

    feat: add choose players service

--- pkg/utils/array.go
+++ pkg/utils/array.go
@@ -7,3 +7,12 @@ type ArrayInterface[Type any] interface {
 func Slice[Type any](arr []Type, startIdx int, endIdx int) []Type {
 	return append(arr[:startIdx], arr[endIdx:]...)
 }
+
+func Contains[Type comparable](arr []Type, value Type) bool {
+	for _, item := range arr {
+		if value == item {
+			return true
+		}
+	}
+	return false
+}
