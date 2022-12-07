9cc2b68399114ad42741c8d12564b5c6e70afb87
JohnKGS <johnkelvinsouza@hotmail.com>
2022-12-07 15:21:39 +0000

    chore: run prettify

--- cartola_fc/asgi.py
+++ cartola_fc/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cartola_fc.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cartola_fc.settings")
 
 application = get_asgi_application()
