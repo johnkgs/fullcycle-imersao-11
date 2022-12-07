481a9aaf5c763f6407c60e40afb3dd8bf5cee7f6
JohnKGS <johnkelvinsouza@hotmail.com>
2022-12-07 17:12:06 +0000

    feat: add action model

--- app/admin.py
+++ app/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from .models import Match, MyTeam, Player, Team
+from .models import Action, Match, MyTeam, Player, Team
 
 # Register your models here.
 admin.site.register(Player)
 admin.site.register(Team)
 admin.site.register(MyTeam)
 admin.site.register(Match)
+admin.site.register(Action)
