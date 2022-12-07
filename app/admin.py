56362df43c94444159e3c68c4630089b7bc8240f
JohnKGS <johnkelvinsouza@hotmail.com>
2022-12-07 15:52:41 +0000

    feat: add my team model

--- app/admin.py
+++ app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Player, Team
+from .models import MyTeam, Player, Team
 
 # Register your models here.
 admin.site.register(Player)
 admin.site.register(Team)
+admin.site.register(MyTeam)
