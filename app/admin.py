8968027fc957e98b76260035a177ebdbe0d4b004
JohnKGS <johnkelvinsouza@hotmail.com>
2022-12-07 16:01:39 +0000

    feat: add match model

--- app/admin.py
+++ app/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from .models import MyTeam, Player, Team
+from .models import Match, MyTeam, Player, Team
 
 # Register your models here.
 admin.site.register(Player)
 admin.site.register(Team)
 admin.site.register(MyTeam)
+admin.site.register(Match)
