4828d906efc62982f79c755f91b241a83a48e4a3
JohnKGS <johnkelvinsouza@hotmail.com>
2022-12-11 03:09:16 +0000

    fix: correct problems

--- cmd/cartola/main.go
+++ cmd/cartola/main.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/confluentinc/confluent-kafka-go/kafka"
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/johnkgs/imersao11-consolidation/internal/infra/config/envs"
 	"github.com/johnkgs/imersao11-consolidation/internal/infra/db"
@@ -37,6 +38,16 @@ func main() {
 
 func initServer(ctx context.Context, dtb *sql.DB) {
 	router := chi.NewRouter()
+	router.Use(cors.Handler(cors.Options{
+		// AllowedOrigins:   []string{"https://foo.com"}, // Use this to allow specific origin hosts
+		AllowedOrigins: []string{"https://*", "http://*"},
+		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	}))
 	router.Get("/players", httpHandler.ListPlayersHandler(ctx, *db.New(dtb)))
 	router.Get("/my-teams/{teamID}/players", httpHandler.ListMyTeamPlayersHandler(ctx, *db.New(dtb)))
 	router.Get("/my-teams/{teamID}/balance", httpHandler.GetMyTeamBalanceHandler(ctx, *db.New(dtb)))
