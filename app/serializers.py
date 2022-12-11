e3f4ca324cc764ffd50a9b0bf99e380285587335
JohnKGS <johnkelvinsouza@hotmail.com>
2022-12-10 22:54:45 -0300

    feat: add update players serializer

---
+++ app/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+
+from .models import Player
+
+
+class UpdateMyPlayersSerializer(serializers.Serializer):
+    players_id = serializers.PrimaryKeyRelatedField(
+        many=True, queryset=Player.objects.all()
+    )
